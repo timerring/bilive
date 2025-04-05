d462988936bb46692cb2b264024325c9b164bacb
John Howe <89397553+timerring@users.noreply.github.com>
2025-04-05 20:13:11 +0800

    chore: format

--- src/cover/image_model_sdk/kolors_sdk.py
+++ src/cover/image_model_sdk/kolors_sdk.py
@@ -4,6 +4,7 @@ import time
 import os
 from src.config import SILICONFLOW_API_KEY
 
+
 def kolors_generate_cover(your_file_path):
     """Generater cover image using SiliconFlow api of Kolors(Kwai)
     Args:
@@ -12,7 +13,7 @@ def kolors_generate_cover(your_file_path):
         str, local download path of the generated cover image file
     """
     with open(your_file_path, "rb") as image_file:
-        data = base64.b64encode(image_file.read()).decode('utf-8')
+        data = base64.b64encode(image_file.read()).decode("utf-8")
 
     payload = {
         "model": "Kwai-Kolors/Kolors",
@@ -21,11 +22,11 @@ def kolors_generate_cover(your_file_path):
         "batch_size": 1,
         "num_inference_steps": 20,
         "guidance_scale": 7.5,
-        "image": f"data:image/webp;base64,{data}"
+        "image": f"data:image/webp;base64,{data}",
     }
     headers = {
         "Authorization": f"Bearer {SILICONFLOW_API_KEY}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     url = "https://api.siliconflow.cn/v1/images/generations"
     response = requests.request("POST", url, json=payload, headers=headers)
@@ -34,13 +35,14 @@ def kolors_generate_cover(your_file_path):
         img_data = requests.get(image_url).content
         cover_name = time.strftime("%Y%m%d%H%M%S") + ".png"
         temp_cover_path = os.path.join(os.path.dirname(your_file_path), cover_name)
-        with open(temp_cover_path, 'wb') as handler:
+        with open(temp_cover_path, "wb") as handler:
             handler.write(img_data)
         os.remove(your_file_path)
         return temp_cover_path
     else:
         print(response.text, flush=True)
         return None
+
 
 if __name__ == "__main__":
     your_file_path = ""
